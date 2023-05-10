08621d15b961ceae23f283e63fbf37b5da68e2a2
Амалия Тайлер <AATayler@1cbit.ru>
2023-05-10 18:11:02 +0300

    added tabs

---
+++ js/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', () => {
+    /* выбор способа получения товара */
+    const tabsBlock = document.querySelector('.tabs-block__tabs'),
+          tabs = tabsBlock.querySelectorAll('.tab'),
+          tabsContent = document.querySelector('.tabs-block__content'),
+          tabsForm = tabsContent.querySelectorAll('.form');
+
+    function hideTabContent() {
+        tabsForm.forEach(form=> {
+            form.style.display = 'none';
+        });
+
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        });
+    }
+
+    /* i указываем по умолчанию 0 */
+    function showTabContent(i = 0) {
+        tabsForm[i].style.display = 'block';
+        tabs[i].classList.add('active');
+    }
+
+    tabsBlock.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('tab')) {
+            tabs.forEach((item, i) => {
+                if(target === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+});
